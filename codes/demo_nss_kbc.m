c974af27e5703bba8a01a00e573ada512f8723e0
forleast330 <49986719+forleast330@users.noreply.github.com>
2025-10-17 15:46:01 +0800

    Update demo_nss_kbc.m

--- codes/demo_nss_kbc.m
+++ codes/demo_nss_kbc.m
@@ -10,7 +10,8 @@ s = min(size(data,1),1000); % sample size
 sig = 1;                    % parameter of Guassian kernel
 ndata = sNstr(data,t,sig)
 sID = randperm(size(ndata,1),s);
-K = ndata(sID,:)*ndata(sID,:)'; % Kernel matrix
-Kn=0.35;                        % parameter of KBC $\tau$
-Tclass = EC_sample(ndata,K,Kn,k,sID); 
+S = ndata(sID,:)*ndata(sID,:)'; % Kernel matrix
+tau=0.35;                        % parameter of KBC $\tau$
+Tclass = EC_sample(ndata,S,tau,k,sID); 
+
 
